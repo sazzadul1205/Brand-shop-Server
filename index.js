48b87196ab820a08c97f397b3b83b9e60405a014
sazzadul1205 <psazzadul@gmail.com>
2023-10-19 00:58:21 +0600

    connected to DB

--- index.js
+++ index.js
@@ -10,6 +10,34 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// access
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@brand-shop.n91hcdj.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
 
 
 
@@ -20,6 +48,3 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Brand Server is Running on Port : ${port}`);
 });
-
-// Sazzadul
-// dNwgebGO2idkEHM0
