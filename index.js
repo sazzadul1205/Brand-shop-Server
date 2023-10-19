dc06c6ba6c6f69729c5f5d83693244767aa507a0
sazzadul1205 <psazzadul@gmail.com>
2023-10-19 14:00:50 +0600

    brand link showing

--- index.js
+++ index.js
@@ -11,8 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@brand-shop.n91hcdj.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@brand-shop-server.zab0nqb.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -31,29 +30,67 @@ async function run() {
     // database 
     const ProductCollection = client.db('productDB').collection('product');
 
+
     // view all products
     app.get('/product', async (req, res) => {
-        const cursor = ProductCollection.find()
-        const result = await cursor.toArray()
-        res.send(result);
+      const cursor = ProductCollection.find()
+      const result = await cursor.toArray()
+      res.send(result);
     })
 
     // view individual products
-    app.get('/product/:id', async (req, res) => {
-        const id = req.params.id;
-        const query = { _id: new ObjectId(id) }
-        const result = await ProductCollection.findOne(query)
-        res.send(result);
-    })
+    // app.get('/product/:id', async (req, res) => {
+    //   const id = req.params.id;
+    //   const query = { _id: new ObjectId(id) }
+    //   const result = await ProductCollection.findOne(query)
+    //   res.send(result);
+    // })
 
     // upload a item to DB
     app.post('/product', async (req, res) => {
-        const newCoffee = req.body;
-        const result = await ProductCollection.insertOne(newCoffee);
-        res.send(result);
+      const newCoffee = req.body;
+      const result = await ProductCollection.insertOne(newCoffee);
+      res.send(result);
     })
 
 
+
+    // view brands
+    // samsung
+    app.get('/product/samsung', async (req, res) => {
+        const cursor = ProductCollection.find();
+        const result = await cursor.toArray();
+        const samsungProducts = result.filter(product => product.brand == 'Samsung');
+        res.send(samsungProducts);
+    });
+    // Apple
+    app.get('/product/apple', async (req, res) => {
+        const cursor = ProductCollection.find();
+        const result = await cursor.toArray();
+        const appleProducts = result.filter(product => product.brand == 'Apple');
+        res.send(appleProducts);
+    });
+    // Asus
+    app.get('/product/asus', async (req, res) => {
+        const cursor = ProductCollection.find();
+        const result = await cursor.toArray();
+        const asusProducts = result.filter(product => product.brand == 'Asus');
+        res.send(asusProducts);
+    });
+    // Oppo
+    app.get('/product/oppo', async (req, res) => {
+        const cursor = ProductCollection.find();
+        const result = await cursor.toArray();
+        const oppoProducts = result.filter(product => product.brand == 'Oppo');
+        res.send(oppoProducts);
+    });
+    // Nokia
+    app.get('/product/nokia', async (req, res) => {
+        const cursor = ProductCollection.find();
+        const result = await cursor.toArray();
+        const NokiaProducts = result.filter(product => product.brand == 'Nokia');
+        res.send(NokiaProducts);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -68,10 +105,10 @@ run().catch(console.dir);
 
 // Set up the basic route
 app.get('/', (req, res) => {
-    res.send('Brand Server is Running');
+  res.send('Brand Server is Running');
 });
 
 // Listen on the specified port
 app.listen(port, () => {
-    console.log(`Brand Server is Running on Port: ${port}`);
+  console.log(`Brand Server is Running on Port: ${port}`);
 });
