b758ea807cd3850d537f807696604b1a46faf0cd
sazzadul1205 <psazzadul@gmail.com>
2023-10-19 18:40:40 +0600

    product update call compleat

--- index.js
+++ index.js
@@ -38,7 +38,67 @@ async function run() {
       res.send(result);
     })
 
+
     // view individual products
+
+
+    // insert a item to DB
+    app.post('/product', async (req, res) => {
+      const newCoffee = req.body;
+      const result = await ProductCollection.insertOne(newCoffee);
+      res.send(result);
+    })
+
+
+
+
+
+
+    // view brands
+    // samsung
+    app.get('/product/samsung', async (req, res) => {
+      const cursor = ProductCollection.find();
+      const result = await cursor.toArray();
+      const samsungProducts = result.filter(product => product.brand == 'Samsung');
+      res.send(samsungProducts);
+    });
+    // Apple
+    app.get('/product/apple', async (req, res) => {
+      const cursor = ProductCollection.find();
+      const result = await cursor.toArray();
+      const appleProducts = result.filter(product => product.brand == 'Apple');
+      res.send(appleProducts);
+    });
+    // Asus
+    app.get('/product/asus', async (req, res) => {
+      const cursor = ProductCollection.find();
+      const result = await cursor.toArray();
+      const asusProducts = result.filter(product => product.brand == 'Asus');
+      res.send(asusProducts);
+    });
+    // Oppo
+    app.get('/product/oppo', async (req, res) => {
+      const cursor = ProductCollection.find();
+      const result = await cursor.toArray();
+      const oppoProducts = result.filter(product => product.brand == 'Oppo');
+      res.send(oppoProducts);
+    });
+    // Nokia
+    app.get('/product/nokia', async (req, res) => {
+      const cursor = ProductCollection.find();
+      const result = await cursor.toArray();
+      const NokiaProducts = result.filter(product => product.brand == 'Nokia');
+      res.send(NokiaProducts);
+    });
+    // Nokia
+    app.get('/product/intel', async (req, res) => {
+      const cursor = ProductCollection.find();
+      const result = await cursor.toArray();
+      const IntelProducts = result.filter(product => product.brand == 'intel');
+      res.send(IntelProducts);
+    });
+
+    // products
     app.get('/product/samsung/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) }
@@ -76,58 +136,163 @@ async function run() {
       res.send(result);
     })
 
-    // upload a item to DB
-    app.post('/product', async (req, res) => {
-      const newCoffee = req.body;
-      const result = await ProductCollection.insertOne(newCoffee);
+    // Update a Product
+    app.put('/product/samsung/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedProduct = req.body;
+      const samsung = {
+        $set: {
+          name: updatedProduct.name,
+          brand: updatedProduct.brand,
+          type: updatedProduct.type,
+          price: updatedProduct.price,
+          rating: updatedProduct.rating,
+          supply: updatedProduct.supply,
+          description: updatedProduct.description,
+          photo: updatedProduct.photo,
+          ram: updatedProduct.ram,
+          rom: updatedProduct.rom,
+          battery: updatedProduct.battery,
+          display: updatedProduct.display,
+
+        }
+      };
+      const result = await ProductCollection.updateOne(filter, samsung, options);
       res.send(result);
-    })
+    });
+    // 
+    app.put('/product/apple/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedProduct = req.body;
+      const samsung = {
+        $set: {
+          name: updatedProduct.name,
+          brand: updatedProduct.brand,
+          type: updatedProduct.type,
+          price: updatedProduct.price,
+          rating: updatedProduct.rating,
+          supply: updatedProduct.supply,
+          description: updatedProduct.description,
+          photo: updatedProduct.photo,
+          ram: updatedProduct.ram,
+          rom: updatedProduct.rom,
+          battery: updatedProduct.battery,
+          display: updatedProduct.display,
 
+        }
+      };
+      const result = await ProductCollection.updateOne(filter, samsung, options);
+      res.send(result);
+    });
+    // 
+    app.put('/product/oppo/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedProduct = req.body;
+      const samsung = {
+        $set: {
+          name: updatedProduct.name,
+          brand: updatedProduct.brand,
+          type: updatedProduct.type,
+          price: updatedProduct.price,
+          rating: updatedProduct.rating,
+          supply: updatedProduct.supply,
+          description: updatedProduct.description,
+          photo: updatedProduct.photo,
+          ram: updatedProduct.ram,
+          rom: updatedProduct.rom,
+          battery: updatedProduct.battery,
+          display: updatedProduct.display,
 
+        }
+      };
+      const result = await ProductCollection.updateOne(filter, samsung, options);
+      res.send(result);
+    });
+    // 
+    app.put('/product/asus/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedProduct = req.body;
+      const samsung = {
+        $set: {
+          name: updatedProduct.name,
+          brand: updatedProduct.brand,
+          type: updatedProduct.type,
+          price: updatedProduct.price,
+          rating: updatedProduct.rating,
+          supply: updatedProduct.supply,
+          description: updatedProduct.description,
+          photo: updatedProduct.photo,
+          ram: updatedProduct.ram,
+          rom: updatedProduct.rom,
+          battery: updatedProduct.battery,
+          display: updatedProduct.display,
 
-    // view brands
-    // samsung
-    app.get('/product/samsung', async (req, res) => {
-        const cursor = ProductCollection.find();
-        const result = await cursor.toArray();
-        const samsungProducts = result.filter(product => product.brand == 'Samsung');
-        res.send(samsungProducts);
+        }
+      };
+      const result = await ProductCollection.updateOne(filter, samsung, options);
+      res.send(result);
     });
-    // Apple
-    app.get('/product/apple', async (req, res) => {
-        const cursor = ProductCollection.find();
-        const result = await cursor.toArray();
-        const appleProducts = result.filter(product => product.brand == 'Apple');
-        res.send(appleProducts);
+    // 
+    app.put('/product/intel/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedProduct = req.body;
+      const samsung = {
+        $set: {
+          name: updatedProduct.name,
+          brand: updatedProduct.brand,
+          type: updatedProduct.type,
+          price: updatedProduct.price,
+          rating: updatedProduct.rating,
+          supply: updatedProduct.supply,
+          description: updatedProduct.description,
+          photo: updatedProduct.photo,
+          ram: updatedProduct.ram,
+          rom: updatedProduct.rom,
+          battery: updatedProduct.battery,
+          display: updatedProduct.display,
+
+        }
+      };
+      const result = await ProductCollection.updateOne(filter, samsung, options);
+      res.send(result);
     });
-    // Asus
-    app.get('/product/asus', async (req, res) => {
-        const cursor = ProductCollection.find();
-        const result = await cursor.toArray();
-        const asusProducts = result.filter(product => product.brand == 'Asus');
-        res.send(asusProducts);
+    // 
+    app.put('/product/nokia/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedProduct = req.body;
+      const samsung = {
+        $set: {
+          name: updatedProduct.name,
+          brand: updatedProduct.brand,
+          type: updatedProduct.type,
+          price: updatedProduct.price,
+          rating: updatedProduct.rating,
+          supply: updatedProduct.supply,
+          description: updatedProduct.description,
+          photo: updatedProduct.photo,
+          ram: updatedProduct.ram,
+          rom: updatedProduct.rom,
+          battery: updatedProduct.battery,
+          display: updatedProduct.display,
+
+        }
+      };
+      const result = await ProductCollection.updateOne(filter, samsung, options);
+      res.send(result);
     });
-    // Oppo
-    app.get('/product/oppo', async (req, res) => {
-        const cursor = ProductCollection.find();
-        const result = await cursor.toArray();
-        const oppoProducts = result.filter(product => product.brand == 'Oppo');
-        res.send(oppoProducts);
-    });
-    // Nokia
-    app.get('/product/nokia', async (req, res) => {
-        const cursor = ProductCollection.find();
-        const result = await cursor.toArray();
-        const NokiaProducts = result.filter(product => product.brand == 'Nokia');
-        res.send(NokiaProducts);
-    });
-    // Nokia
-    app.get('/product/intel', async (req, res) => {
-        const cursor = ProductCollection.find();
-        const result = await cursor.toArray();
-        const IntelProducts = result.filter(product => product.brand == 'intel');
-        res.send(IntelProducts);
-    });
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
